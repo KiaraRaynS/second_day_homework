54c50ff3d1a4988e78259bef021537ac0cb98d94
KiaraRaynS <sherman.rayn@outlook.com>
2016-05-17 16:09:11 -0400

    Bugs in Computer Guesses fixed

--- computer_guessing.py
+++ computer_guessing.py
@@ -1,34 +1,43 @@
 import random
 
-random_number = random.randint(1,100)
-
 
 def computer_guesses():
     game_mode = str(input('Mode: Easy or Hard? '))
-    if game_mode == str('Hard'):
+    if game_mode.lower() == str('hard'):
         x = 1
         y = 100
+        a = 1
+        b = 100
     else:
         x = 1
         y = 10
+        a = 1
+        b = 10
     guesses_count = 0
+    random_number = random.randint(x, y)
     while guesses_count < 5:
-        computer_guess = random.randint(x,y)
+        computer_guess = random.randint(a, b)
         if computer_guess > random_number:
             print('Computer guess {} too large.'.format(computer_guess))
             print('Guesses remaining: '+ str(4 - guesses_count))
-            y = computer_guess - 1
             guesses_count +=1
+            if computer_guess == 1:
+                b = 1
+            else:
+                b = int(computer_guess - 1)
         elif computer_guess < random_number:
             print('Computer guess {} too small.'.format(computer_guess))
             print('Guesses remaining: '+ str(4 - guesses_count))
-            x = computer_guess + 1
             guesses_count +=1
+            if computer_guess == 10:
+                a = 10
+            else:
+                a = int(computer_guess + 1)
         elif computer_guess == random_number:
             print("The computer has correctly guessed the number: {}".format(random_number))
             break
 
 run_question = str(input('Do you wish have computer play guessing game? Y/N '))
 run_game = str('Y')
-if run_question == run_game:
+if run_question.upper() == run_game:
     computer_guesses()
