5936f1ebee065779ca4331af263d893679262f83
KiaraRaynS <sherman.rayn@outlook.com>
2016-05-17 15:28:58 -0400

    Computer made more efficient

--- computer_guessing.py
+++ computer_guessing.py
@@ -5,18 +5,23 @@ random_number = random.randint(1,100)
 
 def computer_guesses():
     guesses_count = 0
-    while guesses_count < 4:
-        computer_guess = random.randint(1,100)
+    x = 1
+    y = 100
+    while guesses_count < 5:
+        computer_guess = random.randint(x,y)
         if computer_guess > random_number:
             print('Computer guess {} too large.'.format(computer_guess))
             print('Guesses remaining: '+ str(4 - guesses_count))
+            y = computer_guess - 1
             guesses_count +=1
         elif computer_guess < random_number:
             print('Computer guess {} too small.'.format(computer_guess))
             print('Guesses remaining: '+ str(4 - guesses_count))
+            x = computer_guess + 1
             guesses_count +=1
         elif computer_guess == random_number:
             print("The computer has correctly guessed the number: {}".format(random_number))
+            break
 
 run_question = str(input('Do you wish have computer play guessing game? Y/N '))
 run_game = str('Y')
